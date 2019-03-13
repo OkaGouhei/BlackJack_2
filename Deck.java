ea3210ce785e973881664a2a1d5c2d83de022ca9
OkaGouhei <g.oka1975@gmail.com>
2019-03-13 19:18:42 +0900

    修正
    card_sizeのような値が不変のものはコンストラクタの外、クラスの中に書いてstatic final int card_size = 52;
    とした方がよいです。
    static(すべてのインスタンスで共通の値を持つ）
    final(値の変更を不可能にする)

--- Deck.java
+++ Deck.java
@@ -1,8 +1,8 @@
 class Deck{
-  private int card_size;
+  static final int card_size=52;
   private boolean[] card;
   public Deck(){
-    card_size = 52;
+    // card_size = 52;
     card = new boolean[card_size];
     for(int i =0;i < card_size;i++){
       card[i] =true;
